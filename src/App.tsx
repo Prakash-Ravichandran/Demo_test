65de1103a6e7004e5bb072811ea1a902e79891ff
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2025-09-06 21:09:24 +0530

    [chore]: add typescript support, remove .vscode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import HomePage from "./components/homepage";
 
 const App = () => {
   return (
     <div>
       <h1>Shapienz staging site!</h1>
+      <HomePage />
     </div>
   );
 };
